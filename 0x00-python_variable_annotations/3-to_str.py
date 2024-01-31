f2c052425369f291ead3a7a2324acfe134537144
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-01-31 12:06:03 +0200

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function that takes float and returns string"""
+
+
+def to_str(n: float) -> str:
+    """function that takes float and returns string"""
+    return str(n)
