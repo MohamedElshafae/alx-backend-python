e52c8e998609a6d802e8f37a0b0715c1ec414fe8
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-02-02 15:46:27 +0200

    done

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,5 +7,5 @@ task_wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """waits for a random delay between 0 and max_delay and returns it"""
+    """waits for a random delay between 0 and max_delay"""
     return sorted(await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n))))
