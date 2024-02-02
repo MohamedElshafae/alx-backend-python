b2d1e17552128b47909f7c641d15cd220cd7ca02
Mohamed Elshafae <mohamedelshafae888@gmail.com>
2024-02-02 14:30:25 +0200

    fix error

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ from asyncio import sleep
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    """waits for a random delay between 0 and max_delay and returns it"""
     delay = uniform(0, max_delay)
     await sleep(delay)
     return delay
